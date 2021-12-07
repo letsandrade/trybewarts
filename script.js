930534722faee68aff46a567cffa5da68733b5c4
leticiaandrade <leticia933@gmail.com>
2021-12-06 22:26:21 -0300

    passando linter requisito 20

--- script.js
+++ script.js
@@ -19,11 +19,10 @@ agreement.addEventListener('click', () => {
 // Referencia: https://htmldom.dev/count-the-number-of-characters-of-a-textarea/
 const textArea = document.getElementById('textarea');
 const counter = document.getElementById('counter');
-counter.innerHTML = '500';
+counter.innerHTML = '500 caracteres restantes';
 textArea.addEventListener('input', (event) => {
-  const target = event.target;
-  const maxLength = target.getAttribute('maxlength');
-  const currentLength = target.value.length;
+  const maxLength = event.target.getAttribute('maxlength');
+  const currentLength = event.target.value.length;
   const currentNumber = maxLength - currentLength;
-  counter.innerHTML = currentNumber;
+  counter.innerHTML = `${currentNumber} caracteres restantes`;
 });
